36eea37adb5b951fd1ea2bea0f0287dd921e2976
Enis Shabani <shabanienis11@outlook.com>
2026-05-18 22:29:53 +0200

    Update

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000', // Porti ku po ekzekutohet backend-i
+  baseURL: '',
 });
 
 // Interceptor për të dërguar tokenin në çdo kërkesë
